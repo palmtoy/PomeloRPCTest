93df7fc93d5c46418c7020ebbcc9d4bab9613871
palmtoy <zgli@corp.netease.com>
2013-12-24 17:03:19 +0800

    * set sendInterval to 1ms

--- client/app/script/lord.js
+++ client/app/script/lord.js
@@ -372,7 +372,7 @@ var monitor = function(type, name, reqId) {
 
 var sum = 10000
   , n = 0
-  , sendInterval = 2;
+  , sendInterval = 1;
 
 var startSend = function(pomelo){
   var route = 'connector.echoHandler.echo';
